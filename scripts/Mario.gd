1ebd677e02e00f5af33519a9c84788a14ddecf9a
brunopapait <brunohpapait@gmail.com>
2020-09-21 23:16:35 -0300

    Corrigindo bugs

--- scripts/Mario.gd
+++ scripts/Mario.gd
@@ -25,7 +25,6 @@ func _physics_process(delta):
 	
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
-			$Sprite.play("pulando");
 			movimento.y = -500;
 	else:
 		$Sprite.play("caindo");
